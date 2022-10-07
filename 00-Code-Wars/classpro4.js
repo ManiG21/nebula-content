b0626aff8ae9fdd9c471077297f34581257a93ad
ManiG21 <manigrant023@gmail.com>
2022-10-06 21:41:59 -0400

    Code Wars 4.4

--- 00-Code-Wars/classpro4.js
+++ 00-Code-Wars/classpro4.js
@@ -17,6 +17,14 @@ function arithmetic(a, b, operator){
     }
   }
 
+   //or
+
+   const arithmetic = (a, b, operator) => {
+    return  ( 
+      operator === "add" ? a + b : operator === "subtract" ? a - b : operator === "multiply" ? a * b : a / b
+    )
+   }
+
 //   You are going to be given a word. Your job is to return the middle character of the word. 
 //   If the word's length is odd, return the middle character.
 //    If the word's length is even, return the middle 2 characters.
@@ -31,3 +39,5 @@ function arithmetic(a, b, operator){
       }
     }
   }
+
+ 
