2e2321e77a3eb47b1e33fa328e5483f7db710293
ManiG21 <manigrant023@gmail.com>
2022-10-07 10:10:49 -0400

    Code Wars 4.4

--- 00-Code-Wars/classpro4.js
+++ 00-Code-Wars/classpro4.js
@@ -40,4 +40,10 @@ function arithmetic(a, b, operator){
     }
   }
 
- 
+//   Create a function that returns the sum of the two lowest positive numbers
+//    given an array of minimum 4 positive integers. 
+//   No floats or non-positive integers will be passed.
+function SumTwoSmallestNumbers(numbers){
+let min = ( => parseInt(numbers.spliceInumbers.indexOf(Math.min(...numbers)),1))
+return min() + min()
+}
