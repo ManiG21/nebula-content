4021a8d67b30e1b55589295285c8dcc7a98a2045
ManiG21 <manigrant023@gmail.com>
2022-10-19 19:24:25 -0400

    Code Wars 5.43

--- 00-Code-Wars/classpro5.js
+++ 00-Code-Wars/classpro5.js
@@ -59,6 +59,9 @@ function disemvowel(str) {
   return str;
 }
 
+// Check to see if a string has the same amount of 'x's and 'o's. 
+// The method must return a boolean and be case insensitive. 
+// The string can contain any char.
 
 function XO(str) {
   let x = 0
