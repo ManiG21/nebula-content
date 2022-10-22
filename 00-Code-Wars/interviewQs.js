b772ab8caecd070e7cf654f5583eaabd26473fd9
ManiG21 <manigrant023@gmail.com>
2022-10-22 13:49:43 -0400

    interviewQs 1

---
+++ 00-Code-Wars/interviewQs.js
@@ -0,0 +1,20 @@
+// -- S1 Theory - answer these questions high level:
+
+//     1 - What is a data structure? why is it important?
+
+// A format to store data,allows us to access and modify them. Each data structure has access to unique properties and
+// methods.
+
+//   2 - What is the definition of a higher order function? What can they be used for? Name 3 different ones.
+
+// A higher order is a function that takes a function as an argument, or returns a function.
+//  method.map(),.reduce(), .sort()
+
+ //     3 - What is a full stack application? Name an example of a tech stack used that can be used create one.
+
+//  A full stack application include both front end(visual aesetic) and backend frameworks + Database. React
+
+ //     4 - What is react? What is one advatange has over vanilla javascript?
+// A framwork built ontop of Javacript that utilizes a virtual/shadow to rerender as few elements as possible
+//  DOM It is more effcient (reduce the load speed of web pages), Doesn't have to reload 
+
