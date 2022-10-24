c6300f0bc1731cc48e32e5cf76a1acb43ae4e2d4
ManiG21 <manigrant023@gmail.com>
2022-10-24 19:02:14 -0400

    Class Pro 6.01

--- 00-Code-Wars/classpro6.js
+++ 00-Code-Wars/classpro6.js
@@ -1,2 +1,11 @@
 // Complete the function that takes a sequence of numbers as single parameter. 
 // Your function must return the sum of the even values of this sequence.
+
+function sumEvenNumbers(input) {
+    let evens = input.filter(num => num % 2 === 0)
+   let sum = evens.reduce(
+    (prev, cur) => prev + cur,
+    0);
+    return sum
+  }
+  
