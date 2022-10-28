e211881f12fd8c71a72323126fd7f57b02467dc1
ManiG21 <manigrant023@gmail.com>
2022-10-27 23:32:18 -0400

    Class Pro 6.2

--- 00-Code-Wars/classpro6.js
+++ 00-Code-Wars/classpro6.js
@@ -49,3 +49,8 @@ function sumEvenNumbers(input) {
     }
     return giftCount
   }
+
+//   Don't give me five!
+// In this kata you get the start number and the end number of a region and 
+// should return the count of all numbers except numbers with a 5 in it.
+// \ The start and the end number are both inclusive!
