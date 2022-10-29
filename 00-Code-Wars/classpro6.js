cd2e45135f62abd970a726670bc7d8f1fe89517b
ManiG21 <manigrant023@gmail.com>
2022-10-28 22:53:38 -0400

    Class Pro 6.21

--- 00-Code-Wars/classpro6.js
+++ 00-Code-Wars/classpro6.js
@@ -54,3 +54,26 @@ function sumEvenNumbers(input) {
 // In this kata you get the start number and the end number of a region and 
 // should return the count of all numbers except numbers with a 5 in it.
 // \ The start and the end number are both inclusive!
+
+
+function dontGiveMeFive(start, end){
+  //inputs : starting and ending indexes
+  // output: count of numbers that dont have a 5 in it
+  // create a loop
+  // let i = start
+  // i <= end
+  // number to a string using toString()
+  // so we can use the includes string method
+  // create a variable to count numbers
+  // create condition/ if statement
+  // check to see if there isnt a "5" in it and then count it
+  // return the count 
+    let count = 0
+  for (let i = start; i < end.length; i++){
+    let numStr = end[i].toString()
+    if(!numStr[i].includes('5')){
+      count++
+    }    
+  } return count
+  
+}
