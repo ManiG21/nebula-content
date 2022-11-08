2cd19ec923e265663b6052a9f39e93c86cc9257f
ManiG21 <manigrant023@gmail.com>
2022-11-07 19:35:55 -0500

    class pro 7.1

--- 00-Code-Wars/classpro7.js
+++ 00-Code-Wars/classpro7.js
@@ -1,1 +1,18 @@
 // When provided with a String, capitalize all vowels
+
+function swap (str) {
+    // input: string
+    // output: sting with capital vowels
+    // Loop over string or turn into Array and use array method (map, join, split)
+    // check to see its a vowel and then capitalize them
+    // create variable for new string
+    // return new string 
+    let split = ''
+  for(let i = 0; i < str.length; i++){
+    if(str[i] === 'a'|| str[i] === 'e'|| str[i] === 'i'|| str[i] === 'o'|| str[i] === 'u'){
+     split += str[i].toUpperCase()
+    }else{
+    split +=  str[i]
+    }
+  }return split
+  }
