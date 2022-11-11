81eeae47dcc6798ed39c529acbd07f490e10fb95
ManiG21 <manigrant023@gmail.com>
2022-11-10 23:38:30 -0500

    classpro 7

--- 00-Code-Wars/hw4.js
+++ 00-Code-Wars/hw4.js
@@ -1,2 +1,9 @@
 // Complete the square sum function so that it squares each number 
 // passed into it and then sums the results together.
+
+function squareSum(numbers){
+    const sqr = numbers.map(x => x * x).reduce( (prev, curr) => prev + curr, 0);
+      return sqr
+    }
+
+    
