9a77da9d41ba568ae8cacdd80f9d75b756c70418
ManiG21 <manigrant023@gmail.com>
2022-11-11 22:57:41 -0500

    classpro 7.11

--- 00-Code-Wars/classpro7.js
+++ 00-Code-Wars/classpro7.js
@@ -37,4 +37,8 @@ function swap (str) {
        return even[0]
      }
   }
+
+  // Complete the method/function so that it converts dash/underscore delimited words into camel casing. 
+  // The first word within the output should be capitalized only if the original word was capitalized 
+  // (known as Upper Camel Case, also often referred to as Pascal case). The next words should be always capitalized.
   
