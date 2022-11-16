f678d4d3666d5fae0e31ddaa3c675151c1d2454d
ManiG21 <manigrant023@gmail.com>
2022-11-15 20:48:30 -0500

    Class Pro 7.15

--- 00-Code-Wars/classpro7.js
+++ 00-Code-Wars/classpro7.js
@@ -79,3 +79,18 @@ function swap (str) {
     // Reverse every other word in a given string, then return the string. 
     // Throw away any leading or trailing whitespace, while ensuring there is exactly one space between each word. 
     // Punctuation marks should be treated as if they are a part of the word in this kata.
+
+    function reverse(str){
+      let strArr = str.split(' ')
+      let fin = ''
+      for (let i = 0; i < strArr.length; i++){
+        if(i % 2 !== 0){
+         fin +=  strArr[i].split('').reverse().join('') + ' '
+        } else {
+         fin +=  strArr[i] + ' '
+        }
+      } 
+       return fin.trim()
+     }
+
+     //or
