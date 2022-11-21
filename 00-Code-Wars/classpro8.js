652d87cb2298b8629ad5fe7a67d571a0cc71a3df
ManiG21 <manigrant023@gmail.com>
2022-11-20 20:56:59 -0500

    classpro8.1

--- 00-Code-Wars/classpro8.js
+++ 00-Code-Wars/classpro8.js
@@ -1,2 +1,17 @@
 // Write an algorithm that takes an array and
 //  moves all of the zeros to the end, preserving the order of the other elements.
+
+function moveZeros(arr) {
+    //input: array
+    //output :array
+    //Loop through to find the zeros
+    let length = arr.length
+    for(let i = 0; length-- ; i++){
+      if(arr[i] === 0){
+        arr.push(arr[i])
+        arr.splice(i, 1)
+        i--
+      }
+    }
+   return arr
+  }
