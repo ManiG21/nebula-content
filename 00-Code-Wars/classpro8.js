6c48d7928202f626836a19f0f1c3be04dff96b88
ManiG21 <manigrant023@gmail.com>
2022-11-22 20:35:01 -0500

    classpro8.21

--- 00-Code-Wars/classpro8.js
+++ 00-Code-Wars/classpro8.js
@@ -25,3 +25,31 @@ function moveZeros(arr) {
 // If the final result is longer than 140 chars it must return false.
 // If the input or the result is an empty string it must return false.
 
+function generateHashtag (str) {
+    //   input: string
+    //   output: string 
+    //   make condition if the final result is longer than 140 characters will return false
+      if(str === "" || !str.trim()){
+        return false;
+      }
+      //   need new variable to store new string with hashtag in it
+      let hashGen = "#"
+    //   for loop to iterate thru string
+      let words = str.split(" ")
+    //   console.log(words, str)
+      for (let i = 0; i < words.length; i++){
+        if(!words[i]){
+          continue
+        }
+    //   need to iterate thru array and capitalize the first letter in words
+    //     .slice
+    //    console.log(words[i][0].toUpperCase(), words[i].slice(1).toLowerCase())
+    //     console.log(words[i])
+        hashGen += words[i][0].toUpperCase() + words[i].slice(1).toLowerCase()
+    //     console.log(hashGen)
+      }
+      if(hashGen.length > 140){
+        return false
+      }
+      return hashGen
+    }
