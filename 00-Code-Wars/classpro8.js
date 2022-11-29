3cd130aa02d0eb4c43774bf20b43d540dc703f2f
ManiG21 <manigrant023@gmail.com>
2022-11-28 21:21:14 -0500

    8.31

--- 00-Code-Wars/classpro8.js
+++ 00-Code-Wars/classpro8.js
@@ -52,6 +52,8 @@ function generateHashtag (str) {
       return hashGen
     }
 
+    // Define a method/function that removes from a given array of integers 
+    // all the values contained in a second array.
 
     Array.prototype.remove_ = function(integer_list, values_list){
       return integer_list.filter(x => {
