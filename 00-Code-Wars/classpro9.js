2528a1c705238ffd655bad6e8008dbb917a63be0
ManiG21 <manigrant023@gmail.com>
2022-12-12 22:34:13 -0500

    class pro 9

--- 00-Code-Wars/classpro9.js
+++ 00-Code-Wars/classpro9.js
@@ -3,3 +3,18 @@
 // Rules: Obviously the words should be Caps, Every word should end with '!!!!',
 //  Any letter 'a' or 'A' should become '@', Any other vowel should become '*'.
 
+function addArrays(arr1, arr2) {
+    // input: Array 
+    // Output: Array 
+   // Check to see if two array are equal in length
+    let newArr = []
+    if(arr1.length !== arr2.length){
+        throw new Error('invalid length')
+      }
+    for(let i = 0; i < arr1.length; i++){
+       if(arr1.length === arr2.length){
+        newArr.push(arr1[i] + arr2[i])
+      }
+    }
+    return newArr
+  }
