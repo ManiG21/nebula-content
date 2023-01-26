b73c08424def5b7da4be93d5b7892e496e8dd1d4
ManiG21 <manigrant023@gmail.com>
2023-01-25 23:30:11 -0500

    corrections

--- 00-Code-Wars/classpro.js
+++ 00-Code-Wars/classpro.js
@@ -1,7 +1,5 @@
 // Return an array containing the numbers from 1 to N, where N is the parametered value.
-
 // Replace certain values however if any of the following conditions are met:
-
 // If the value is a multiple of 3: use the value "Fizz" instead
 // If the value is a multiple of 5: use the value "Buzz" instead
 // If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
@@ -128,12 +126,13 @@ return [min,max]
 
 function minMax(arr) {
   let min = arr[0]
-  for(let i = 1; i < arr.length; i++)
-  const num = arr[i]
+  for(let i = 1; i < arr.length; i++){
+  let num = arr[i]
   if(num < min){
     min = num
   }
 return [min,max]
+}
 }
 
 // Write function which takes a string and make an acronym of it.
