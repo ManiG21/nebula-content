9c5f9cd819d68ec492e534f085edc99800c4b213
ManiG21 <manigrant023@gmail.com>
2023-02-09 22:43:54 -0500

    clean up

--- 00-Code-Wars/classpro.js
+++ 00-Code-Wars/classpro.js
@@ -138,7 +138,6 @@ return [min,max]
 // Write function which takes a string and make an acronym of it.
 
 // Rule of making acronym in this kata:
-
 // split string to words by space char
 // take every first letter from word in given string
 // uppercase it
