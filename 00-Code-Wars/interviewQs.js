dd949759c78fba1b85f6cf065cc02481d26a1da2
ManiG21 <manigrant023@gmail.com>
2023-02-14 22:59:37 -0500

    new q

--- 00-Code-Wars/interviewQs.js
+++ 00-Code-Wars/interviewQs.js
@@ -47,3 +47,8 @@ console.log('--Section 2--');
  
   let x = 'str'
   console.log(x === 'str') //[value],
+
+//   There is a singly-linked list head and we want to delete a node node in it.
+// You are given the node to be deleted node. You will not be given access to the first node of head.
+// All the values of the linked list are unique, and it is guaranteed that the given node node is not the last node in the linked list.
+// Delete the given node. Note that by deleting the node, we do not mean removing it from memory.
