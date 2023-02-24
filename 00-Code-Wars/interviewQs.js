20f079694bcd707c97dbc7b86fc9c4da4eb685ea
ManiG21 <manigrant023@gmail.com>
2023-02-23 23:25:18 -0500

    node answ

--- 00-Code-Wars/interviewQs.js
+++ 00-Code-Wars/interviewQs.js
@@ -52,3 +52,8 @@ console.log('--Section 2--');
 // You are given the node to be deleted node. You will not be given access to the first node of head.
 // All the values of the linked list are unique, and it is guaranteed that the given node node is not the last node in the linked list.
 // Delete the given node. Note that by deleting the node, we do not mean removing it from memory.
+
+let deleteNode = function(node) {
+  node.val = node.next.val
+  node.next = node.next.next
+}
