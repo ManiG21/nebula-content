2fc0a0352215b4a7f2243481c121e6a6b525f844
ManiG21 <manigrant023@gmail.com>
2023-03-24 23:59:53 -0400

    new

--- 01-My-Review/review2.js
+++ 01-My-Review/review2.js
@@ -1,7 +1,20 @@
 // Control Flow And Booleans
 
+// Comparison Operators
+
 5 < 10 // returns true
 
 'b' > 'a' // returns true
 
 7 < '14' // returns true *This is type Coercion*
+
+50 <= 100 // returns true
+
+50 >= 100 // returns false
+
+'The One' === 'The One' // returns true
+//
+
+5 === '5' // returns false
+
+5 !== 5 // returns false
