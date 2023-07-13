fa028f9624ee149aedeeeb3e541f156eaa59b485
ManiG21 <manigrant023@gmail.com>
2023-07-13 15:56:12 -0400

    functionality

--- 01-My-Review/To-Do List review/review.js
+++ 01-My-Review/To-Do List review/review.js
@@ -8,6 +8,7 @@ function addTask(){
     }
     else{
         let li = document.createElement("li");
-        
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
     }
 }
