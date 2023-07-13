38fbfefb3891bd182a3eeeca611a52019ac248b4
ManiG21 <manigrant023@gmail.com>
2023-07-13 15:54:30 -0400

    list style 1.1

---
+++ 01-My-Review/To-Do List review/review.js
@@ -0,0 +1,13 @@
+const inputBox = document.getElementById("input-box")
+const listContainer = document.getElementById("list-container")
+
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You need to write a task.");
+    }
+    else{
+        let li = document.createElement("li");
+        
+    }
+}
