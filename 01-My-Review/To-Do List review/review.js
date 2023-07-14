ddcb87ef3dcb629b23ca1878e88012cb47a80e40
ManiG21 <manigrant023@gmail.com>
2023-07-13 23:11:59 -0400

    functionality

--- 01-My-Review/To-Do List review/review.js
+++ 01-My-Review/To-Do List review/review.js
@@ -3,12 +3,18 @@ const listContainer = document.getElementById("list-container")
 
 
 function addTask(){
+    // Checks if input is empty
     if(inputBox.value === ''){
         alert("You need to write a task.");
     }
     else{
+        // Pushes txt into list
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7"
     }
+    // Clears the input 
+    inputBox.value = ""
 }
