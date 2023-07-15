611572e1d4e005d556ee978c45f82030d35571be
ManiG21 <manigrant023@gmail.com>
2023-07-15 00:22:52 -0400

    fin functionality

--- 01-My-Review/To-Do List review/review.js
+++ 01-My-Review/To-Do List review/review.js
@@ -23,7 +23,7 @@ function addTask(){
 
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
-        e.target.classlist.toggle("checked")
+        e.target.classList.toggle("checked");
     }
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
@@ -38,3 +38,4 @@ function saveData(){
 function showTask(){
     listContainer.innerHTML = localStorage.getItem("data")
 }
+showTask()
