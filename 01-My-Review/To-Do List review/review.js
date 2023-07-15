f2b1f962d6b7c8601fb61c92015284e9a972fac3
ManiG21 <manigrant023@gmail.com>
2023-07-14 23:31:36 -0400

    added X

--- 01-My-Review/To-Do List review/review.js
+++ 01-My-Review/To-Do List review/review.js
@@ -12,9 +12,9 @@ function addTask(){
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
-        // let span = document.createElement("span");
-        // span.innerHTML = "\u00d7";
-        // li.appendChild(span);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
     // Clears the input 
     inputBox.value = "";
