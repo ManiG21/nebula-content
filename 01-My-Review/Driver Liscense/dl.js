e0707bc45aa449fc5c42adc00b249bd6741a0cf1
ManiG21 <manigrant023@gmail.com>
2023-07-29 23:40:27 -0400

    pract

---
+++ 01-My-Review/Driver Liscense/dl.js
@@ -0,0 +1,12 @@
+<div className="App">
+<h1>Mock Liscense </h1>
+<div className="liscense-container">
+  <div className="star">*</div>
+  <div> Full Name</div>
+  <div> Adress </div>
+  <div> Dob </div>
+  <div> Issued </div>
+  <div> Expired </div>
+  <div className="short-bday">Oct 90</div>
+</div>
+</div>
