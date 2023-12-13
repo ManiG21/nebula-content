a7639ef73bb75ab9a740266de1abe5fed1c5113b
ManiG21 <manigrant023@gmail.com>
2023-12-13 00:02:52 -0500

    adjust answers

--- 1-variables/practice/variables.js
+++ 1-variables/practice/variables.js
@@ -10,7 +10,6 @@
   // 3. Set a variable to a boolean value 
   const isOpen = true
   // 4. Create a variable called carName, assign the value Volvo to it.
-  let carName = "Volvo"
   // 5. Create a variable called num, assign the value 50 to it.
   const num = 50 
   // 6. Display the sum of 5 + 10, using two variables: num1 and num2.
